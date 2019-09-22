cf836ec7b73db6e6f591044e121f9ce603d6cb18
Keith Bartholomew <keith.bartholomew@gmail.com>
2019-09-21 20:38:28 -0500

    placeholder united landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import UnitedLogo from "../images/united-logo.svg"
+import TrekLogo from "../images/trek-logo.svg"
+import HTLogo from "../images/hyperthreads-logo.svg"
 import SEO from "../components/seo"
+
+import "./index.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi</h1>
+    <div className="logo-band">
+      <img alt="United Logo" src={UnitedLogo} />
+    </div>
+    <div className="sponsor-band">
+      <img alt="Trek logo" src={TrekLogo} style={{ width: "200px" }} />
+      <img alt="Hyperthreads logo" src={HTLogo} style={{ width: "200px" }} />
+    </div>
+    <div className="info-band">
+      <span>Find us on:</span>
+      <a href="https://www.instagram.com/united__cycling/">Instagram</a>
+      <a href="https://www.instagram.com/united__cycling/">Facebook</a>
+    </div>
   </Layout>
 )
 
